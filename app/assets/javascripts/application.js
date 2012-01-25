ab820a09270f85dde9263fde89384a88a5e5d5fb
Alona Mekhovova <alona.tarasova@gmail.com>
2012-01-25 11:46:13 +0200

    fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,31 +13,30 @@ var Page = {
   init: function() {
     $(".tablesorter").tablesorter(); 
 
-  	//When page loads...
-  	$(".tab_content").hide(); //Hide all content
-	  $("ul.tabs li:first").addClass("active").show(); //Activate first tab
-  	$(".tab_content:first").show(); //Show first tab content
+    //When page loads...
+    $(".tab_content").hide(); //Hide all content
+    $("ul.tabs li:first").addClass("active").show(); //Activate first tab
+    $(".tab_content:first").show(); //Show first tab content
 
-	  //On Click Event
-	  $("ul.tabs li").click(function() {
+    //On Click Event
+    $("ul.tabs li").click(function() {
 
-	  	$("ul.tabs li").removeClass("active"); //Remove any "active" class
-	  	$(this).addClass("active"); //Add "active" class to selected tab
-	  	$(".tab_content").hide(); //Hide all tab content
+      $("ul.tabs li").removeClass("active"); //Remove any "active" class
+      $(this).addClass("active"); //Add "active" class to selected tab
+      $(".tab_content").hide(); //Hide all tab content
 
-	  	var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
-	  	$(activeTab).fadeIn(); //Fade in the active ID content
-	  	return false;
-	  });
+      var activeTab = $(this).find("a").attr("href"); //Find the href attribute value to identify the active tab + content
+      $(activeTab).fadeIn(); //Fade in the active ID content
+      return false;
+    });
 
     $('.column').equalHeight();
   },
-  
+
   showContent: function(content, node) {
     if (typeof node2 === "undefined") node = "section#main";
     $(node).html(content);
   }
-
 }
 
 $(document).ready(Page.init);
