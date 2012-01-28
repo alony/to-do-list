46ab88f83c82dddbf24f0aed9fdde1e11b11ea94
amekhovova <alona.tarasova@gmail.com>
2012-01-28 22:57:18 +0200

    bughunt

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
   def projects_list
-    projects = current_user.projects.map do |project| 
+    projects = current_user.projects.map do |project|
       link_to project.name, project, :class => (current_page?(project_path project) ? "current" : ''), :id => project.id, :remote => true
     end.join("<div class='breadcrumb_divider'></div>")
-    [projects, link_to("Add project", new_project_path, :remote => true)].join("<div class='breadcrumb_divider new'></div>").html_safe
+    [projects, link_to("Add project", new_project_path, :remote => true)].reject(&:blank?).join("<div class='breadcrumb_divider new'></div>").html_safe
   end
 end
+
