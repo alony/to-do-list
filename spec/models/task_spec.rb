8c573f7139359c34e12fe0d6876d176bc6b71c09
Alona Mekhovova <alona.tarasova@gmail.com>
2012-02-02 12:28:22 +0200

    tasks emails test

--- spec/models/task_spec.rb
+++ spec/models/task_spec.rb
@@ -29,8 +29,20 @@ describe Task do
     }.should change{ Task.count }.by 1
   end
   
-#  it "should notify by email on changes" do
-#  end
+  it "should notify by email when assigned" do
+    @assigned = User.create(name: 'A', email: 'a@aa.aa', password: 'qwe123')
+    Task.create valid_attributes.merge(assigned_id: @assigned.id)
+    ActionMailer::Base.deliveries.should_not be_empty
+  end
+  
+  it "should notify by email on changes" do
+    @author = User.create(name: 'S', email: 's@ss.ss', password: 'qwe123')
+    @assigned = User.create(name: 'A', email: 'a@aa.aa', password: 'qwe123')
+    @task = Task.create valid_attributes.merge(assigned_id: @assigned.id, author_id: @author.id)
+    lambda {
+      @task.update_attributes status: :resolved
+    }.should change{ ActionMailer::Base.deliveries.count }
+  end
   
   def valid_attributes
     {name: "feed a cat", desc: "cat food $4.90", list_id: 1, priority: 1, status: :new}
